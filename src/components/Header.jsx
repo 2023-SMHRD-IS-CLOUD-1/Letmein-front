9c27573acb4851340690ab5e5970a06ece1d23da
ksm <king4767@naver.com>
2024-02-02 16:55:13 +0900

    mainpage update and img add - 0202 16:55 -ksm

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
 
     return (
       <div className='header-container'>
-        <img src={logo} alt="Logo" style={{ width: '130px' }} onClick={()=>nav("/")}/>
+        <img src={logo} alt="Logo" style={{ width: '130px', cursor: 'pointer'}} onClick={()=>nav("/")}/>
         <div className="navigation-links">
           <Link to="/upload" >체형 분석</Link>
           <Link to="/community">커뮤니티</Link>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,45 @@
-import React from 'react'
-import main from '../images/Main.jpg'
-import '../css/main.css'
-// 메인화면
+import React, { useState, useEffect } from 'react';
+import main1 from '../images/Main.jpg';
+import main2 from '../images/Main2.png';
+import main3 from '../images/Main3.jpg';
+import '../css/main.css';
+
 const Main = () => {
+  // 이미지 리스트와 현재 인덱스를 상태로 관리
+  const [images] = useState([main1, main2, main3]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // 다음 이미지로 이동하는 함수
+  const goToNextSlide = () => {
+    const nextIndex = (currentIndex + 1) % images.length;
+    setCurrentIndex(nextIndex);
+  };
+
+  useEffect(() => {
+    // 일정 시간마다 다음 이미지로 넘어가는 타이머 설정
+    const interval = setInterval(goToNextSlide, 3000); // 3초마다 변경
+    return () => clearInterval(interval); // 컴포넌트가 언마운트될 때 타이머 제거
+  }, [currentIndex]);
+
   return (
     <div className='main-contaiver'>
       <div style={{ position: 'relative', width: 'auto', marginTop: '20px' }}>
-        <img src={main} style={{ width: '100%' }} alt="Main" />
+        <img src={images[currentIndex]} style={{ width: '100%' }} alt="Main" />
         <p className='overlay-text'>
-           Meet your own<br/>
-           fashion coordinator<br/>
-           at Let Me In
+          Meet your own<br/>
+          fashion coordinator<br/>
+          at Let Me In
         </p>
+        <hr className='overlay-horizon'/>
+        <button className='overlay-button-left' onClick={() => setCurrentIndex((currentIndex - 1 + images.length) % images.length)}>&lt;</button>
+        <button className='overlay-button-right' onClick={goToNextSlide}>&gt;</button>
         <div>
           <h1>FASHION</h1>
           <span></span>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
